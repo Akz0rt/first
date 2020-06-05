1590653a5cdb39024a1e578bc7a222080f06509b
Evhen Dyrda <geka.skype@gmail.com>
2020-06-05 10:58:36 +0300

    Create Teoriya.cpp
    Загальний вигляд шаблону класу:
    template <class C, type1 var1, type2 var2, ..., typeN varN> class ClassName
    {
        // тіло шаблона класу
        // ...
    }
    
    Відповідно аналогічна ситуація і з шаблонними методами. У виконуючій програмі  створюємо об'єкт
    RacNumber <int> mi; // об'єкт mi класу працюватиме з типом int
    Надалі викликаємо для цього об'єкту необхідні методи, що містяться у шаблоні класу, в аргументах методів зазначаємо, з яким числом(числами) ми працюємо

---
+++ Zalik_DyrdaEvhen/Teoriya.cpp
@@ -0,0 +1,37 @@
+template <class N> //створюємо шаблон класу N, що представляє собою клас RacNumber - раціональне число
+class RacNumber
+{
+    public:
+    // конструктор,
+    RacNumber(void) { }
+
+    // метод множення чисел
+    void Mult(N, N);
+
+    // метод підведення числа до квадрату
+    N MySquare(N);
+
+    // метод, що відповідає за ділення
+    N DivNumbers(N, N);
+};
+
+// шаблонний метод множення двох чисел
+template <class N> void RacNumber<N>::Mult(N t1, T t2)
+{
+    return (N)(t1 * t2);
+}
+
+// шаблонний метод, що повертає квадрат числа
+template <class N> N RacNumber<N>::MySquare(N number)
+{
+    return (N)(number*number);
+}
+
+// шаблон методу ділення двох чисел
+template <class N> N RacNumber<N>::DivNumbers(N t1, T t2)
+{
+    return (N)(t1/t2);
+}
+
+//шаблон методу, що визначає додавання чисел
+template <class N> N RacNumber
